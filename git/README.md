26a381c9e27636063d7b8465cbc66746784d6c57
Joaquim Rocha <joaquim@kinvolk.io>
2020-05-04 12:32:49 +0200

    Add git hooks
    So far the only hook present is the commit-msg one.

---
+++ git/README.md
@@ -0,0 +1,13 @@
+# Git utilities
+
+## Hooks
+
+Current we have the following hooks:
+
+  * `commit-msg`: Checks whether the commit message is compliant with the
+  common commit message rules.
+
+## Installation
+
+`install.sh destination_repo` installs all hooks within the hooks folder into
+the `destination_repo` repository.
---
+++ git/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_ABS_PATH="$(realpath $0)"
+SRC="$(dirname $SCRIPT_ABS_PATH)"
+HOOKS_SRC=$SRC/hooks
+DEST=$1
+
+function help {
+  echo "Usage `basename $0` TARGET_REPO_BASE_DIR"
+}
+
+if [ -z $DEST ]; then
+  printf "Please provide a target repository.\n\n"
+  help
+  exit 1
+fi
+
+HOOKS_DEST=$DEST/.git/hooks
+
+if [ ! -d $HOOKS_DEST ]; then
+  echo "No git folder found in ${HOOKS_DEST}"
+  exit 1
+fi
+
+hooks=$(find $HOOKS_SRC -type f)
+for hook in ${hooks[@]}; do
+  hook_name=$(basename $hook)
+  hook_src_path="$HOOKS_SRC/$hook_name"
+  hook_dest_path="$HOOKS_DEST/$hook_name"
+  if [ -L $hook_dest_path ]; then
+    echo "Not installing $hook_src_path: $hook_dest_path already exists!"
+  elif [ -e $hook_dest_path ]; then
+    echo "Not installing $hook_src_path: $hook_dest_path already exists!"
+  else
+    printf "Installing $hook_src_path\t->\t$hook_dest_path\n"
+    ln -s "$hook_src_path" $hook_dest_path
+  fi
+done
